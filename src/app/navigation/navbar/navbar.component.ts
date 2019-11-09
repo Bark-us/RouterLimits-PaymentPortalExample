bce569b90c833d866f8bba21bf2b89f1cbb6ae6f
Hytea <andrew@routerlimits.com>
2019-11-08 17:08:59 -0700

    Mobile app menu (#8)
    * Optimizing for mobile app
    
    * Hide nav option

--- src/app/navigation/navbar/navbar.component.ts
+++ src/app/navigation/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/authentication/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,12 +9,17 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  hideNav: boolean;
+
+  constructor(private router: Router, private authService: AuthService) {
+    this.hideNav = this.authService.isInApp;
+   }
 
   @Input() backText = 'Back';
   @Input() routeUrl = '/billing/home';
   @Input() noReturn = false;
   ngOnInit() {
+    this.hideNav = this.authService.isInApp;
   }
 
   navArrowClicked() {
